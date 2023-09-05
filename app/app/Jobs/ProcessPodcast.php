f6e43763e4df5d8d79395eeca47707d78165e45c
Juan Paganuzzi <paganuzzi@gmail.com>
2023-09-04 22:34:01 -0300

    init commit

---
+++ app/app/Jobs/ProcessPodcast.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\OrderShipped;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class ProcessPodcast implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $randomNumber = rand(5, 30);
+        Mail::to('paganuzzi@gmail.com')->queue(new OrderShipped($randomNumber));
+    }
+}
